98437aa707c154dce1143947983878d6a8670df1
Ryan Campbell <sachel.campbell@gmail.com>
2023-03-25 11:56:47 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -29,25 +29,25 @@ chkfile = os.scandir('./')
 
 print(str(chkfile))
 
-# if scraped in chkfile:
+if scraped in chkfile:
 
-#     '''Send Email'''
-#     '''For password you have to enable 2-factor on gmail and then set a
-#         App Password for mail - Other'''
-#     port = 465  # For SSL
-#     smtp_server = "smtp.gmail.com"
-#     sender_email = os.getenv('SENDER_EMAIL')  # Enter your address
-#     receiver_email = "sachel.campbell@gmail.com"  # Enter receiver address
-#     password = os.getenv('PASSWORD')
-#     message = """\
-#             Subject: New Web Scrape (Alert!!!)
+    '''Send Email'''
+    '''For password you have to enable 2-factor on gmail and then set a
+        App Password for mail - Other'''
+     port = 465  # For SSL
+     smtp_server = "smtp.gmail.com"
+     sender_email = os.getenv('SENDER_EMAIL')  # Enter your address
+     receiver_email = "sachel.campbell@gmail.com"  # Enter receiver address
+     password = os.getenv('PASSWORD')
+     message = """\
+             Subject: New Web Scrape (Alert!!!)
 
-#             This message is to inform you that your scrape was successful.
-#             The scrape occured at: """ + str(datetime.now())
+             This message is to inform you that your scrape was successful.
+             The scrape occured at: """ + str(datetime.now())
 
-#     context = ssl.create_default_context()
-#     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-#         server.login(sender_email, password)
-#         server.sendmail(sender_email, receiver_email, message)
-# else:
-#     print("Error")
+     context = ssl.create_default_context()
+     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+         server.sendmail(sender_email, receiver_email, message)
+ else:
+     print("Error")
